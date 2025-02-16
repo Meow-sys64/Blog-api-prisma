0e1437b7daa208f42635dd64c43d06734e333932
Meow-sys64 <g6vhytX1Mgipaz@proton.me>
2025-02-16 06:17:25 -0500

    create updateBloggerStatus route and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -100,5 +100,37 @@ module.exports = {
         return res.status(500).json({ success: false, message: "Server Error when issuing token" })
       }
     }],
+  updateBloggerStatus: [
+    body("secretCode")
+      .escape(),
+
+    async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, errorArray: errors.array() })
+      }
+console.log(req.user);
+
+      // validate secretCode
+      // update isBlogger status
+      try {
+        await prisma.user.update({
+          where: {
+            id: req.user.id
+          },
+          data: {
+            isBlogger: true
+          }
+        })
+        res.status(200).json({success: true, message:"User given blogger privileges"})
+      }
+      catch (err) {
+        console.error(err)
+        res.status(500).json({ success: false, message: 'Server Error when updating user' })
+      }
+    }
+  ],
+  deleteUser: async (req, res, next) => {
+
   },
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router  = express.Router()
 const userController = require("../controllers/userController")
+const passport = require("passport")
 
 router.post("/register", userController.register )
 router.post("/login", userController.login )
+
+router.put("/bloggerStatus", passport.authenticate("jwt",{session:false}),  userController.updateBloggerStatus)
 
 router.delete("/", userController.deleteUser)
 
