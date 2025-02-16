22a1c6338bd99f8ca292ca96ef9e6bf772ce74b0
Meow-sys64 <g6vhytX1Mgipaz@proton.me>
2025-02-16 06:14:59 -0500

    create register controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,59 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
-const { body, validationResult } = require('express-validator')
+const { body, validationResult } = require('express-validator');
+const { genHash, validateHash, issueJWT } = require('../lib/Utils');
 
 module.exports = {
-  register: [ 
+  register: [
     body("username")
-      .notEmpty(),
+      .notEmpty()
+      .escape()
+      .withMessage("Error with username format"),
+    body("password")
+      .notEmpty()
+      .withMessage("Password cannot be empty")
+      .isLength({ min: 5 })
+      .withMessage("Password must be at least 5 characters long")
       .escape(),
-      .withMessage()
-    async (req,res,next)=>{
 
-  } ],
-  login: async (req,res,next)=>{
+    async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, errorArray: errors.array() })
+      }
+      const { username, password } = req.body
 
-  },
-  deleteUser: async (req,res,next)=>{
+      //check if username exists
+      const checkName = await prisma.user.findUnique({
+        where: {
+          username: username
+        }
+      })
+
+      if (checkName) {
+        res.status(400).json({ message: "Username already exists", })
+      }
+
+      //create account
+      const passwordHash = await genHash(password)
+      try {
+        const user = await prisma.user.create({
+          data: {
+            username: username,
+            password_hash: passwordHash
+          }
+        })
+        const jwt = issueJWT(user)
+        return res.status(200).json({ success: true, token: jwt })
+
+      }
+      catch (err) {
+        console.error(err)
+        return res.status(500).json({ success: false, message: "Server error when creating the account" })
+      }
+
+
+    }],
 
   },
 }
